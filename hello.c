a4da5f2be3acf77de23efd41551d557f8569d446
mitesh-gohel <63446371+mitesh-gohel@users.noreply.github.com>
2020-04-10 14:38:37 +0530

    add funB

--- hello.c
+++ hello.c
@@ -3,10 +3,15 @@ void fun1()
 {
   printf ("fun1()\n");
 }
+void funB()
+{
+  printf ("funB()\n");
+}
 int main()
 {
   printf("Hello world \n");
   printf("How are you! \n");
   fun1();
+  funB();
   return 0;
 }
